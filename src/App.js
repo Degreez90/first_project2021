3768d93aa65a4166c23eebf8e40948dd6ee7787e
Offitt, Mark Sheridan <moffitt@iu.edu>
2021-02-10 21:37:19 -0500

    Updated Name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello my name is Mark.
+          Hello my name is Mark Offitt.
         </p>
         <a
           className="App-link"
